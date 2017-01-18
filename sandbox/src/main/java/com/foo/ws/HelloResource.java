a76cf9d2dbabe1112be73d45268e140d0d583ad1
kubamarchwicki <kuba@marchwicki.pl>
2017-01-18 10:57:59 +0100

    Response object

--- sandbox/src/main/java/com/foo/ws/HelloResource.java
+++ sandbox/src/main/java/com/foo/ws/HelloResource.java
@@ -5,6 +5,10 @@ package com.foo.ws;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.OutputStream;
 
 @Path("/hello")
 public class HelloResource {
@@ -17,11 +21,33 @@ public class HelloResource {
 
     @GET
     @Path("/{name}")
-    public String sayHello(@PathParam("name") String name) {
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public HelloMessage sayHello(@PathParam("name") String name) {
         if (name != null || name.isEmpty())
-            return "Hello, " + name;
+            return new HelloMessage("Hello, " + name);
 
         throw new IllegalStateException("Name is null!!!");
+    }
+
+    @GET
+    @Path("/secured")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response method() {
+
+        return Response.noContent()
+                .header("X-ContentType", "application/pdf")
+                .build();
+    }
+
+
+    public static class HelloMessage {
+
+        public final String message;
+
+        public HelloMessage(String msg) {
+            this.message = msg;
+        }
+
     }
 
 }
