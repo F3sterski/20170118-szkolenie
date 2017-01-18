08bc7c995269f664e67cf1f33e0b5b5cfccfefd1
kubamarchwicki <kuba@marchwicki.pl>
2017-01-18 07:33:47 +0100

    Cwiczenie 1: tresc

--- projekt/services-jersey/profanity/src/test/java/ws/rest/ProfanityResourceTest.java
+++ projekt/services-jersey/profanity/src/test/java/ws/rest/ProfanityResourceTest.java
@@ -1,13 +1,20 @@
 package ws.rest;
 
 import org.glassfish.jersey.test.JerseyTest;
-import org.json.JSONObject;
 import org.junit.Test;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import java.io.StringReader;
+import java.net.URISyntaxException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ProfanityResourceTest extends JerseyTest {
 
@@ -20,8 +27,12 @@ public class ProfanityResourceTest extends JerseyTest {
     public void should_return_escaped_phrase() {
         final String phrase = "This is shit";
 
-        final String result = target("profanity").path(phrase).request().get(String.class);
-        JSONObject object = new JSONObject(result);
+        final String result = target("profanity").path(phrase).request()
+                .accept(MediaType.APPLICATION_JSON_TYPE)
+                .get(String.class);
+
+        JsonReader reader = Json.createReader(new StringReader(result));
+        JsonObject object = reader.readObject();
 
         assertThat(object.getBoolean("containsProfanity"), equalTo(true));
         assertThat(object.getString("input"), equalTo(phrase));
@@ -32,14 +43,28 @@ public class ProfanityResourceTest extends JerseyTest {
     public void should_return_only_input() {
         final String phrase = "This is ok";
 
-        final String result = target("profanity").path(phrase).request().get(String.class);
-        JSONObject object = new JSONObject(result);
+        final String result = target("profanity").path(phrase).request()
+                .accept(MediaType.APPLICATION_JSON_TYPE)
+                .get(String.class);
+        JsonReader reader = Json.createReader(new StringReader(result));
+        JsonObject object = reader.readObject();
 
         assertThat(object.getBoolean("containsProfanity"), equalTo(false));
         assertThat(object.getString("input"), equalTo(phrase));
+        assertThat(object.containsKey("output"), equalTo(false));
+    }
 
-//        TODO: uncomment this and make the test pass
-//        assertThat(object.has("output"), equalTo(false));
+    @Test
+    public void should_return_escaped_xml_phrase() throws URISyntaxException {
+        final String input = "This is fucked";
+
+        WebTarget target = target().path("profanity").path(input);
+        String xmlString = target.request()
+                .accept(MediaType.APPLICATION_XML_TYPE)
+                .get(String.class);
+
+        assertTrue(xmlString.contains("<containsProfanity>true</containsProfanity>"));
+        assertTrue(xmlString.contains("<output>This is ******</output>"));
     }
 
 }
